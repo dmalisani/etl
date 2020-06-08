a870c11a6d035c1365be62b7096c5ee8c8d64d3b
Daniel Malisani <dmalisani@plexar.tech>
2020-06-08 02:08:28 -0300

    it send batches to each process

---
+++ data_processor/api_conector.py
@@ -0,0 +1,33 @@
+import requests
+
+
+def get_token():
+    return "token"
+
+
+def generate_token_cokie():
+    pass
+
+
+def write_token_cokie(token):
+    pass
+
+
+def retrieve_token_cokie():
+    return "mi_koquie"
+
+
+def get_from_items():
+    pass
+
+
+def get_from_categories():
+    pass
+
+
+def get_from_currencies():
+    pass
+
+
+def get_from_users():
+    pass
